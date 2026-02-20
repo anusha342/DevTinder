2bf20b2b05cf3a7a91c2901125994faa2dbb49ad
Anusha Raj <anusharaj1804@gmail.com>
2026-02-20 14:36:49 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -19,6 +19,42 @@ app.post("/signup",async(req, res) => {
     }
 });
 
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+
+    try{
+        const user = await User.findOne({ emailId: userEmail });
+        res.send(user);
+
+        if(!user){
+            res.status(404).send("User not found");
+        }
+        else{
+            res.send(user);
+        }
+        // const users = await User.find({ emailId: userEmail });
+        // if(users.length === 0){
+        //     res.status(404).send("User not found");
+        // }
+        // else{
+        //     res.send(users);
+        // }
+    }
+    catch(err){
+        res.status(400).send("Something went wrong");
+    }
+});
+//Feed API - GET /feed -get all the users from the database
+app.get("/feed", async(req, res) => {
+  try{
+    const users = await User.find({});
+    res.send(users);
+  }
+  catch(err){
+        res.send(400).send("Something went wrong");
+    }
+});
+
 
 connectDB()
      .then(() => {
