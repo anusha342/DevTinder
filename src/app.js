a1d8684267a6a7d54afc762039ca823b6153e665
Anusha Raj <anusharaj1804@gmail.com>
2026-03-06 22:29:21 +0530

    create login API

--- src/app.js
+++ src/app.js
@@ -4,8 +4,31 @@ const app = express();
 const User = require("./models/user");
 const {validateSignUpData} = require("./utils/validation");
 const bcrypt = require('bcrypt');
+const { isPassportNumber } = require('validator');
 
 app.use(express.json());
+
+app.post("/login", async(req, res)=>{
+    try{
+       const{emailId, password} = req.body;
+
+       const user = await User.findOne({emialId: emailId});
+       if(!User){
+        throw new Error("Invalid credentials");
+       }
+       const isPasswordValid = await bcrypt.compare(password, user.password);
+
+       if(isPasswordValid){
+        res.send("Login Successfully!!");
+       }
+       else{
+        throw new Error("Invalid credentials");
+       }
+    }
+    catch(err){
+     res.status(400).send("ERROR : "+ err.message);
+    }
+})
 
 app.post("/signup",async(req, res) => {
    
